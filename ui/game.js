3920bc25508951707a50a69a30a0d64d030a9231
james-e-crandall <james.e.crandall@gmail.com>
2025-05-30 21:17:36 -0500

    Finished Version

--- ui/game.js
+++ ui/game.js
@@ -4,6 +4,10 @@ window.UnitPortraitVisualRemaps = {
         unitType: 'UNIT_MERCHANT',
         background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
     },
+    'UNIT_TRANS_SAHARAN_TRADER_2': { //your unit type
+        unitType: 'UNIT_MERCHANT',
+        background: "UnitPortraitsBG_UNIQUE", //"UnitPortraitsBG_UNIQUE" || "UnitPortraitsBG_BASE"
+    },
 }
 
 
